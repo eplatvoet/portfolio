c323ac42ae94f8a40f342f37c2e8e5f0116daed1
Erin Platvoet <erin.didomenico520@gmail.com>
2020-09-18 00:03:46 -0400

    modal info updated and second modal added

--- assets/extras/script.js
+++ assets/extras/script.js
@@ -1,14 +1,14 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById("modalOne");
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
+var img = document.getElementById("poke");
+var modalImg = document.getElementById("pokedex");
+var captionTextOne = document.getElementById("captionOne");
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
+  captionTextOne.innerHTML = this.alt;
 }
 
 // Get the <span> element that closes the modal
@@ -16,5 +16,25 @@ var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() { 
-  modal.style.display = "none";
+    modalOne.style.display = "none";
+  }
+
+
+// Get the modal
+var modalTwo = document.getElementById("modalTwo");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var imgTwo = document.getElementById("grub");
+var modalImgTwo = document.getElementById("localGrub");
+var captionTextTwo = document.getElementById("captionTwo");
+imgTwo.onclick = function(){
+  modalTwo.style.display = "block";
+  modalImgTwo.src = this.src;
+  captionTextTwo.innerHTML = this.alt;
+}
+
+var spanTwo = document.getElementsByClassName("close")[1];
+
+spanTwo.onclick = function() {
+  modalTwo.style.display = "none";
 }
