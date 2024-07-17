59e577655121a4e81df29b900bc46e07ce48a898
Erin Platvoet <67918985+eplatvoet@users.noreply.github.com>
2024-07-17 15:56:02 -0400

    adding cert-modal funct & cleaning files up

--- assets/extras/script.js
+++ assets/extras/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    // MODAL FUNCTIONALITY
+    // PORTFOLIO MODAL FUNCTIONALITY
     $('.portfolio-thumbnails').click(function(){
         var imgSrc = $(this).attr('src');
         $('.modal-img').attr('src', imgSrc);
@@ -19,18 +19,57 @@ $(document).ready(function(){
             $('#dep-link').attr('href', deployedLink);
         }
 
-        $('#project-modal').css('display', 'block');
+        $('#project-modal').css('display', 'flex');
     });
 
     $('.close').click(function(){
-        $('#project-modal').css('display', 'none');
+        if($('#project-modal').css('display') === "flex"){
+            $('#project-modal').css('display', 'none');
+        }
+        if($('#certificates-modal').css('display') === "flex"){
+            $('#certificates-modal').css('display', 'none');
+        }
     });
 
     $(window).click(function(event){
         if ($(event.target).is('#project-modal')) {
             $('#project-modal').css('display', 'none');
         }
+        if ($(event.target).is('#certificates-modal')) {
+            $('#certificates-modal').css('display', 'none');
+        }
     });
 
-    // 
+    // CERTIFICATES MODAL FUNCTIONALITY
+    $('.certificates-modal-trigger').click(function(){
+        $('#certificates-modal').css('display', 'flex')
+        function updateCarousel(direction) {
+            var $activeItem = $('.carousel-item.active');
+            var $newActiveItem;
+
+            if (direction === 'left') {
+                $newActiveItem = $activeItem.prev('.carousel-item');
+                if ($newActiveItem.length === 0) {
+                    $newActiveItem = $('.carousel-item').last();
+                }
+            } else {
+                $newActiveItem = $activeItem.next('.carousel-item');
+                if ($newActiveItem.length === 0) {
+                    $newActiveItem = $('.carousel-item').first();
+                }
+            }
+
+            $activeItem.removeClass('active');
+            $newActiveItem.addClass('active');
+        }
+
+        $('.c-trig.left-trigger').click(function() {
+            updateCarousel('left');
+        });
+
+        $('.c-trig.right-trigger').click(function() {
+            updateCarousel('right');
+        });
+        
+    })
 });
